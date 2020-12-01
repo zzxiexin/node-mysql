f5d464cafc95af5c93926578a86e74edf38768d9
yuanxinxin <yuanxinxin@111.com.cn>
2020-12-01 17:51:39 +0800

    'curd尝试'

--- routes/curd.js
+++ routes/curd.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var connectDB = require('../server/index')
-/* GET home page. */
-router.get('/', async function (req, res, next) {
-    const data = await connectDB('SELECT * from accout')
+let id = 2;
+router.get('/get/:id', async function (req, res, next) {
+    const sql = `SELECT * from account where qq = ${req.params.id}`
+    const data = await connectDB(sql)
     return res.json(data)
+});
+
+router.post('/add_qq', async function (req, res, next) {
+    const { qq, mima } = req.body
+    const sql = `INSERT INTO account (id, qq, mima) VALUES (${id++}, ${qq}, ${mima});`
+    const data = await connectDB(sql)
+    console.log(data)
+});
+
+router.delete('/del_qq', async function (req, res, next) {
+    const { qq } = req.body
+    const sql = `DELETE FROM account WHERE qq = ${qq};`
+    const data = await connectDB(sql)
+    console.log(data)
+});
+
+router.put('/update_qq', async function (req, res, next) {
+    const { qq, mima } = req.body
+    const sql = `UPDATE account set qq=${qq}, mima=${mima} where qq = ${qq};`
+    const sql1 = "UPDATE account set qq= '" + qq + "', mima= '" + mima + "' where qq = '" + qq + "';"
+    console.log(sql1)
+    const data = await connectDB(sql1)
+    console.log(data)
 });
 
 module.exports = router;
